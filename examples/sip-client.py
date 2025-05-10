d7ea3056dd3c8e424df033a9a50814ad415c3120
Jeremy Lainé <jeremy.laine@m4x.org>
2025-05-10 23:25:41 +0200

    Add some media handling to examples
    Add command-line options for reading / write media from client or
    server.

--- examples/sip-client.py
+++ examples/sip-client.py
@@ -9,6 +9,7 @@ import logging
 
 import sipmessage
 import sipua
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
 
 
 class SipClient(sipua.Application):
@@ -17,13 +18,30 @@ class SipClient(sipua.Application):
         *,
         local_address: sipmessage.Address,
         remote_address: sipmessage.Address,
+        play_from: str,
+        record_to: str,
     ) -> None:
-        # Make the call.
         call = sipua.Call.create_uac(
             dialog_layer=self.dialog_layer,
             local_address=local_address,
             remote_address=remote_address,
         )
+
+        # Play audio from a file or fall back to silence.
+        if play_from:
+            player = MediaPlayer(play_from)
+            call.audioTransceiver.sender.replaceTrack(player.audio)
+
+        # Consume received audio.
+        recorder: MediaBlackhole | MediaRecorder
+        if record_to:
+            recorder = MediaRecorder(record_to)
+        else:
+            recorder = MediaBlackhole()
+        recorder.addTrack(call.audioTransceiver.receiver.track)
+        await recorder.start()
+
+        # Make the call.
         await call.invite()
 
         # Send media for a few seconds.
@@ -32,12 +50,19 @@ class SipClient(sipua.Application):
         # Hangup the call.
         await call.hangup()
 
+        # Stop recording.
+        await recorder.stop()
+
 
 async def main() -> None:
     parser = argparse.ArgumentParser("sip client")
     parser.add_argument("--local-username", type=str, default="+33122334455")
     parser.add_argument("--local-host", type=str, default="127.0.0.1")
     parser.add_argument("--local-port", type=int, default=5061)
+    parser.add_argument(
+        "--play-from", type=str, help="Read the media from a file and sent it."
+    )
+    parser.add_argument("--record-to", type=str, help="Write received media to a file.")
     parser.add_argument("--remote-username", type=str, default="+33233445566")
     parser.add_argument("--remote-host", type=str, default="127.0.0.1")
     parser.add_argument("--remote-port", type=int, default=5060)
@@ -66,6 +91,9 @@ async def main() -> None:
                 port=options.remote_port,
             )
         ),
+        # Media playback and recording.
+        play_from=options.play_from,
+        record_to=options.record_to,
     )
     await app.close()
     await asyncio.sleep(1)
--- examples/sip-server.py
+++ examples/sip-server.py
@@ -9,20 +9,56 @@ import logging
 
 import sipmessage
 import sipua
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder
+
+
+async def ensure_hangup(
+    call: sipua.Call, recorder: MediaBlackhole | MediaRecorder
+) -> None:
+    await asyncio.sleep(15)
+    await call.hangup()
+    await recorder.stop()
 
 
 class SipServer(sipua.Application):
+    def __init__(self, play_from: str, record_to: str) -> None:
+        super().__init__()
+        self.play_from = play_from
+        self.record_to = record_to
+
     async def handle_request(self, request: sipmessage.Request) -> None:
         """
         Handle a request outside a dialog.
         """
         if request.method == "INVITE":
-            # Accept the incoming call.
             call = sipua.Call.create_uas(
                 dialog_layer=self.dialog_layer,
                 request=request,
             )
+
+            # Play audio from a file or fall back to a sine signal.
+            if self.play_from:
+                player = MediaPlayer(self.play_from)
+            else:
+                player = MediaPlayer(
+                    "sine=frequency=440:sample_rate=48000:duration=1", format="lavfi"
+                )
+            call.audioTransceiver.sender.replaceTrack(player.audio)
+
+            # Consume received audio.
+            recorder: MediaBlackhole | MediaRecorder
+            if self.record_to:
+                recorder = MediaRecorder(self.record_to)
+            else:
+                recorder = MediaBlackhole()
+            recorder.addTrack(call.audioTransceiver.receiver.track)
+            await recorder.start()
+
+            # Accept the incoming call.
             await call.accept(request)
+
+            # Ensure the call is hung up 15s later.
+            asyncio.create_task(ensure_hangup(call, recorder))
         else:
             # Reject any other requests.
             await super().handle_request(request)
@@ -32,11 +68,15 @@ async def main() -> None:
     parser = argparse.ArgumentParser("sip server")
     parser.add_argument("--local-host", type=str, default="127.0.0.1")
     parser.add_argument("--local-port", type=int, default=5060)
+    parser.add_argument(
+        "--play-from", type=str, help="Read the media from a file and sent it."
+    )
+    parser.add_argument("--record-to", type=str, help="Write received media to a file.")
     options = parser.parse_args()
 
     logging.basicConfig(level=logging.INFO)
 
-    server = SipServer()
+    server = SipServer(play_from=options.play_from, record_to=options.record_to)
     await server.listen(
         sipua.TransportAddress("udp", options.local_host, options.local_port)
     )
